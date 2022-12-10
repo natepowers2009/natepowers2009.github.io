fda32267c1c45d93adb3b4af016c93a61d1b7e0d
natepowers2009 <95651912+natepowers2009@users.noreply.github.com>
2022-12-10 11:48:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-Console.log('man, this is a weeeeiiiird timeline, am i right?')
+/*
+Note: I would like to clarify that I am absolutely HORRIBLE at javascript, and just still learning with html and css. 
+The main.js page is one of my major testing grounds for learning javascript.
+I have been learning javascript, html, and css self-taught for a few months now, and have yeilded wonderful results.
+If there is any requests for ideas as to what I should add to this webstie, contact me in person or at nathanpowers2009@gmail.com.
+Thank you for using my website!
+*/
+
+var test_val = math.random()
+
+if (test_val < 1000) {
+  console.log('Less Than Val')
+}else if (test_val > 1000) {
+  console.log('Greater Than Val') 
+}else if (test_val === 1000) {
+  console.log('Lucky, equal to!')
+}else{
+  console.log('ERROR')
+}
