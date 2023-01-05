4daeddf8f232e1a87a619f661908dac7bb2011c0
Zeglol1234 <nathanpowers2009@gmail.com>
2023-01-05 08:46:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
 function counter() {if (typeof counter === 'undefined') {var counter = 1; console.log(counter);}else if (typeof counter === 'number') {counter = ++counter; console.log(counter);} return counter};
+var counter = 0
