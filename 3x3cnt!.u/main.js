39685ff0b2398b32d7d62d2ac5c21041c9d0cc84
Zeglol1234 <nathanpowers2009@gmail.com>
2023-01-06 13:56:30 -0500

    Create main.js

---
+++ 3x3cnt!.u/main.js
@@ -0,0 +1,6 @@
+function 1() {clicks = 0}
+function 2() {if (clicks === 2 || clicks === 11 || clicks === 12 || clicks === 13) {clicks = clicks++}else{clicks = 0}}
+function 3() {if (clicks === 17 || clicks === 18 || clicks === 19) {clicks = clicks++}else{clicks = 0}}
+function 4() {clicks = 0}
+function 5() {clicks = 0}
+function 6() {}
