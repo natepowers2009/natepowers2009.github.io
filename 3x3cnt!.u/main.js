6c3dcc15ce17ef92d6e5f6c58cbca231c7f79dc7
Zeglol1234 <nathanpowers2009@gmail.com>
2023-01-06 16:05:33 -0500

    Update main.js

--- 3x3cnt!.u/main.js
+++ 3x3cnt!.u/main.js
@@ -1,7 +1,7 @@
 let clicks = 1
 function one() {clicks = '1'; console.log(clicks); return clicks;}
 function two() {if (clicks === 2 || clicks === 11 || clicks === 12 || clicks === 13) {clicks = ++clicks}else{clicks = 1}; console.log(clicks); return clicks;}
-function three() {if (clicks === 17 || clicks === 18 || clicks === 19) {if (clicks === 20) {console.log('passcode accepted')}else{++clicks}}else{clicks = 1}; console.log(clicks); return clicks;}
+function three() {if (clicks === 17 || clicks === 18 || clicks === 19) {if (clicks === 20) {console.log('passcode accepted')}else{++clicks}}else{clicks = 1}; console.log(clicks); if (clicks===20) {window.location.replace("https://natepowers2009.github.io/3x3cnt!.u/0d3u");}; return clicks;}
 function four() {clicks = '1'; console.log(clicks); return clicks;}
 function five() {clicks = '1'; console.log(clicks); return clicks;}
 function six() {if (clicks === 14|| clicks === 15 || clicks === 16) {clicks = ++clicks}else{clicks=1}; return clicks;}
