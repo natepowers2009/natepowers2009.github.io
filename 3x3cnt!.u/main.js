4135b9857ca13a994ddfc007aae6eec54df1ca3d
Zeglol1234 <nathanpowers2009@gmail.com>
2023-01-06 14:51:37 -0500

    Update main.js

--- 3x3cnt!.u/main.js
+++ 3x3cnt!.u/main.js
@@ -1,11 +1,11 @@
 let clicks = 1
-function one() {clicks = '1'}
-function two() {if (clicks === 2 || clicks === 11 || clicks === 12 || clicks === 13) {clicks = ++clicks}else{clicks = 1}}
-function three() {if (clicks === 17 || clicks === 18 || clicks === 19) {if (clicks === 20) {console.log('passcode accepted')}else{++clicks}}else{clicks = 1}}
-function four() {clicks = '1'}
-function five() {clicks = '1'}
-function six() {if (clicks === 14|| clicks === 15 || clicks === 16) {clicks = ++clicks}else{clicks=1}}
-function seven() {if (clicks === 1|| clicks === 3|| clicks === 4|| clicks === 5|| clicks === 6|| clicks === 7|| clicks === 8|| clicks === 9|| clicks === 10) {clicks = ++clicks}else{clicks = 1}}
-function eight() {clicks = '1'}
-function nine() {clicks = '1'}
-function zero() {clicks = '1'}
+function one() {clicks = '1'; return clicks;}
+function two() {if (clicks === 2 || clicks === 11 || clicks === 12 || clicks === 13) {clicks = ++clicks}else{clicks = 1}; return clicks;}
+function three() {if (clicks === 17 || clicks === 18 || clicks === 19) {if (clicks === 20) {console.log('passcode accepted')}else{++clicks}}else{clicks = 1}; return clicks;}
+function four() {clicks = '1'; return clicks;}
+function five() {clicks = '1'; return clicks;}
+function six() {if (clicks === 14|| clicks === 15 || clicks === 16) {clicks = ++clicks}else{clicks=1}; return clicks;}
+function seven() {if (clicks === 1|| clicks === 3|| clicks === 4|| clicks === 5|| clicks === 6|| clicks === 7|| clicks === 8|| clicks === 9|| clicks === 10) {clicks = ++clicks}else{clicks = 1}; return clicks;}
+function eight() {clicks = '1'; return clicks;}
+function nine() {clicks = '1'; return clicks;}
+function zero() {clicks = '1'; return clicks}
