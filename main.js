e37abca4a2df6dd1790b6d85e1dc25b8e551aab8
Zeglol1234 <nathanpowers2009@gmail.com>
2023-01-06 09:53:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 function counter() {if (typeof counter === 'undefined') {var counter = 1; console.log(counter);}else if (typeof counter === 'number') {counter = ++counter; console.log(counter);} return counter};
 var counter = 0
+counter()
+if (typeof counter === 'undefined') {console.error('Function Error')}else{console.log('function success')}
