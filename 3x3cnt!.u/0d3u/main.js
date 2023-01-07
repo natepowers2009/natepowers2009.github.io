ec59455d331d80c21202d2dffe64406e6591f92b
Zeglol1234 <nathanpowers2009@gmail.com>
2023-01-07 12:12:13 -0500

    Update main.js

--- 3x3cnt!.u/0d3u/main.js
+++ 3x3cnt!.u/0d3u/main.js
@@ -3,9 +3,9 @@ function one() {if (clicks === 1) {clicks = ++clicks}else{clicks = 1}}
 function two() {if (clicks === 4) {clicks = ++clicks}else{clicks = 1}}
 function three() {if (clicks === 3) {clicks = ++clicks}else{clicks = 1}}
 function four() {if (clicks === 5 || clicks === 6) {clicks = ++clicks}else{clicks = 1}; if (clicks === 7) {window.location.replace("https://natepowers2009.github.io/3x3cnt!.u/0d3u/c0p3-th433/");}}
-function five() {clicks = '1'}
-function six() {clicks= '1'}
-function seven() {clicks = '1'}
-function eight() {clicks = '1'}
+function five() {clicks = 1}
+function six() {clicks= 1}
+function seven() {clicks = 1}
+function eight() {clicks = 1}
 function nine() {if (clicks === 2) {clicks = ++clicks}else{clicks = 1}}
-function zero() {clicks = '1'}
+function zero() {clicks = 1}
