7bd004642e80ac0d32878c4b326edff513df4ca3
Zeglol1234 <nathanpowers2009@gmail.com>
2023-01-07 12:07:21 -0500

    Update main.js

--- 3x3cnt!.u/0d3u/main.js
+++ 3x3cnt!.u/0d3u/main.js
@@ -1,11 +1,11 @@
 let clicks = 1
-function one() {clicks = '1'}
-function two() {if (clicks === 2 || clicks === 11 || clicks === 12 || clicks === 13) {clicks = ++clicks}else{clicks = 1}}
-function three() {if (clicks === 17 || clicks === 18 || clicks === 19) {if (clicks === 20) {console.log('passcode accepted')}else{++clicks}}else{clicks = 1}}
-function four() {clicks = '1'}
+function one() {if (clicks === 1) {clicks = ++clicks}else{clicks = 1}}
+function two() {if (clicks === 4) {clicks = ++clicks}else{clicks = 1}}
+function three() {if (clicks === 3) {clicks = ++clicks}else{clicks = 1}}
+function four() {if (clicks === 5 || clicks === 6) {clicks = ++clicks}else{clicks = 1}; if (clicks === 7) {window.location.replace("https://natepowers2009.github.io/3x3cnt!.u/0d3u/c0p3-th433/");}}
 function five() {clicks = '1'}
-function six() {if (clicks === 14|| clicks === 15 || clicks === 16) {clicks = ++clicks}else{clicks=1}}
-function seven() {if (clicks === 1|| clicks === 3|| clicks === 4|| clicks === 5|| clicks === 6|| clicks === 7|| clicks === 8|| clicks === 9|| clicks === 10) {clicks = ++clicks}else{clicks = 1}}
+function six() {clicks= '1'}
+function seven() {clicks = '1'}
 function eight() {clicks = '1'}
-function nine() {clicks = '1'}
+function nine() {if (clicks === 2) {clicks = ++clicks}else{clicks = 1}}
 function zero() {clicks = '1'}
