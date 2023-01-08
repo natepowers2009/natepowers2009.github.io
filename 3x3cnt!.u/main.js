f998fe3c831f1a3e8606ac5a651301cc7c1506c0
Zeglol1234 <nathanpowers2009@gmail.com>
2023-01-08 15:11:31 -0500

    Update main.js

--- 3x3cnt!.u/main.js
+++ 3x3cnt!.u/main.js
@@ -1,11 +1,11 @@
-let clicks = 1
-function one() {clicks = '1'; console.log(clicks); return clicks;}
-function two() {if (clicks === 2 || clicks === 11 || clicks === 12 || clicks === 13) {clicks = ++clicks}else{clicks = 1}; console.log(clicks); return clicks;}
-function three() {if (clicks === 17 || clicks === 18 || clicks === 19) {if (clicks === 20) {console.log('passcode accepted')}else{++clicks}}else{clicks = 1}; console.log(clicks); if (clicks===20) {window.location.replace("https://natepowers2009.github.io/3x3cnt!.u/0d3u");}; return clicks;}
-function four() {clicks = '1'; console.log(clicks); return clicks;}
-function five() {clicks = '1'; console.log(clicks); return clicks;}
-function six() {if (clicks === 14|| clicks === 15 || clicks === 16) {clicks = ++clicks}else{clicks=1}; console.log(clicks); return clicks;}
-function seven() {if (clicks === 1|| clicks === 3|| clicks === 4|| clicks === 5|| clicks === 6|| clicks === 7|| clicks === 8|| clicks === 9|| clicks === 10) {clicks = ++clicks}else{clicks = 1}; console.log(clicks); return clicks;}
-function eight() {clicks = '1'; console.log(clicks); return clicks;}
-function nine() {clicks = '1'; console.log(clicks); return clicks;}
-function zero() {clicks = '1'; console.log(clicks); return clicks}
+let clicks = 1;
+function one() {clicks = '1'; console.log(clicks); if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')}; return clicks;}
+function two() {if (clicks === 2 || clicks === 11 || clicks === 12 || clicks === 13) {clicks = ++clicks}else{clicks = 1;  if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')};}; console.log(clicks); return clicks;}
+function three() {if (clicks === 17 || clicks === 18 || clicks === 19) {if (clicks === 20) {console.log('passcode accepted')}else{++clicks}}else{clicks = 1;  if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')};}; console.log(clicks); if (clicks===20) {window.location.replace("https://natepowers2009.github.io/3x3cnt!.u/0d3u");}; return clicks;}
+function four() {clicks = '1'; console.log(clicks); if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')}; return clicks;}
+function five() {clicks = '1'; console.log(clicks); if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')}; return clicks;}
+function six() {if (clicks === 14|| clicks === 15 || clicks === 16) {clicks = ++clicks}else{clicks=1; if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')};}; console.log(clicks); return clicks;}
+function seven() {if (clicks === 1|| clicks === 3|| clicks === 4|| clicks === 5|| clicks === 6|| clicks === 7|| clicks === 8|| clicks === 9|| clicks === 10) {clicks = ++clicks}else{clicks = 1;  if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')};}; console.log(clicks); return clicks;}
+function eight() {clicks = '1'; console.log(clicks); if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')}; return clicks;}
+function nine() {clicks = '1'; console.log(clicks); if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')}; return clicks;}
+function zero() {clicks = '1'; console.log(clicks); if (typeof hint === 'undefined') {var hint = 'used'; document.write('<p>Hint: Passcode</p>')}; return clicks}
